2bc980f62596646e375e973135221baa1b138ec7
ChenBeixuan2 <77818087+ChenBeixuan2@users.noreply.github.com>
2021-12-13 20:45:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function setup()
+{
+canvas = createCanvas(400, 400);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechSynthesis;
+}
+
+function clearCanvas() 
+{
+    background("white");
+}
+
+function preload()
+{
+
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw() 
+{
+    strokeWeight(10);
+    stroke(0);
+    if(mouseIsPressed) 
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() 
+{
+classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) 
+{
+    if(error)
+    {
+console.log(error);
+    }
+
+    else 
+    {
+        console.log(results);
+        document.getElementById("label").innerHTML= "label:" + results[0].label;
+        document.getElementById("confidence").innerHTML= "confidence:" + Math.round(results[0].confidence * 100) + '%';
+        utterThis= new SpeechSynthesisUtterance(results[0].label);
+        synth.Speak(utterThis);
+    }
+}
